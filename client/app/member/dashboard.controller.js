6c90efa0a729917b6257ab0f2b818beeb78f21d2
Trevor Van Meter <vanmeter.trevor@gmail.com>
2015-03-05 06:39:12 -0800

    Working on App Layout & Welcome screen

--- client/app/member/dashboard.controller.js
+++ client/app/member/dashboard.controller.js
@@ -3,30 +3,40 @@
 angular.module('HitchedApp')
   .controller('DashboardCtrl', function($scope) {
 
-    $scope.currFeature = 'wedding';
+    $scope.currFeature = 'welcome';
     $scope.currFeatureSrc = 'app/member/features/' + $scope.currFeature + '/' + $scope.currFeature;
 
     // App features
     $scope.features = [{
+      'title': 'Home',
+      'template': 'welcome',
+      'img': '',
+      'class': 'box-primary'
+    },{
       'title': 'Wedding Info',
       'template': 'wedding',
-      'img': ''
-    }, {
-      'title': 'Games',
-      'template': 'games',
-      'img': ''
-    }, {
-      'title': 'Photos',
-      'template': 'photos',
-      'img': ''
-    }, {
-      'title': 'Guests',
-      'template': 'guests',
-      'img': ''
+      'img': '',
+      'class': 'box-primary'
     }, {
       'title': 'Customize App',
       'template': 'customize',
-      'img': ''
+      'img': '',
+      'class': 'box-primary'
+    }, {
+      'title': 'Games',
+      'template': 'games',
+      'img': '',
+      'class': 'box-primary'
+    }, {
+      'title': 'Photos',
+      'template': 'photos',
+      'img': '',
+      'class': 'box-primary'
+    }, {
+      'title': 'Guests',
+      'template': 'guests',
+      'img': '',
+      'class': 'box-primary'
     }];
 
     $scope.selectFeature = function(template) {
