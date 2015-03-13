dcd02955c0e7d342724a1766857176eabf23b656
Trevor Van Meter <vanmeter.trevor@gmail.com>
2015-03-12 20:32:29 -0700

    Working on the main page.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,10 @@
 'use strict';
 
 angular.module('HitchedApp')
-  .controller('MainCtrl', function($scope, $location) {
+  .controller('MainCtrl', function($scope, $location, $modal, $log, Auth) {
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    
     // Add action for the 'scrollTop' element 
     $scope.scrollTop = function() {
       $location.hash('/');
@@ -9,6 +12,23 @@ angular.module('HitchedApp')
         scrollTop: 0
       }, 'slow');
     };
+
+    // Open a modal template
+    $scope.open = function(template, ctrl) {
+      var modalInstance = $modal.open({
+        templateUrl: 'app/account/' + template + '/' + template + '.html',
+        controller: ctrl
+      });
+
+      modalInstance.result.then(function(result) {
+        if (result && result.template) {
+          $scope.open(result.template, result.ctrl);
+        }
+      }, function() {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
   })
   .controller('ContactCtrl', function($scope, $location, ContactUs) {
     $scope.errors = {};
