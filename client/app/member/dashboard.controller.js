3f35c8971039a734fe7a06b141d9def58a7176fe
Trevor Van Meter <vanmeter.trevor@gmail.com>
2015-03-16 06:37:23 -0700

    Working on slider (animation not working)

--- client/app/member/dashboard.controller.js
+++ client/app/member/dashboard.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
-angular.module('HitchedApp')
+angular.module('HitchedApp') //, ['ngAnimate']
   .controller('DashboardCtrl', function($scope) {
 
+    $scope.idx = 0;
     $scope.currFeature = 'welcome';
     $scope.currFeatureSrc = 'app/member/features/' + $scope.currFeature + '/' + $scope.currFeature;
 
@@ -12,37 +13,63 @@ angular.module('HitchedApp')
       'title': 'Home',
       'template': 'welcome',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 0
     },
     {
       'title': 'Wedding Info',
       'template': 'wedding',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 1
     }, {
       'title': 'Games',
       'template': 'games',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 2
     }, {
       'title': 'Customize App',
       'template': 'customize',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 3
     },{
       'title': 'Guests',
       'template': 'guests',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 4
     },  {
       'title': 'Photos',
       'template': 'photos',
       'img': '',
-      'class': 'box-primary'
+      'class': 'box-primary',
+      'idx': 5
     }];
 
-    $scope.selectFeature = function(template) {
+    $scope.selectFeature = function(template, idx) {
+      $scope.idx = idx;
       $scope.currFeature = template;
       $scope.currFeatureSrc = 'app/member/features/' + $scope.currFeature + '/' + $scope.currFeature;
     };
+
+    $scope.nextFeature = function(){
+      var nextIdx = $scope.idx + 1;
+      
+      if(nextIdx < $scope.features.length){
+        var nextFeature = $scope.features[nextIdx];
+        $scope.selectFeature(nextFeature.template, nextFeature.idx); 
+      }     
+    };
+
+    $scope.backFeature = function(){
+      var backIdx = $scope.idx - 1;
+
+      if(backIdx > -1){
+        var nextFeature = $scope.features[backIdx];
+        $scope.selectFeature(nextFeature.template, nextFeature.idx);    
+      }
+    };
+
   });
